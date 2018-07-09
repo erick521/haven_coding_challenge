07852b4b78d25b974e565625adedd12f7d32efcb
EC2 Default User <ec2-user@ip-172-31-19-116.us-west-2.compute.internal>
2018-07-09 13:30:43 +0000

    minor cosmetic change to details form

--- resources/views/modals/contact-details-modal.blade.php
+++ resources/views/modals/contact-details-modal.blade.php
@@ -14,61 +14,61 @@
                     <div class="form-row">
                         <label for="first_name" class="col-sm-4 col-form-label">First Name</label>
                         <div class="col-sm-8">
-                            <input type="text" readonly class="form-control-plaintext" id="first_name" name="first_name" required>
+                            <input style="font-weight: bold;" type="text" readonly class="form-control-plaintext" id="first_name" name="first_name" required>
                         </div>
                     </div>
                     <div class="form-row">
                         <label for="last_name" class="col-sm-4 col-form-label">Last Name</label>
                         <div class="col-sm-8">
-                            <input type="text" readonly class="form-control-plaintext" id="last_name" name="last_name" required>
+                            <input style="font-weight: bold;" type="text" readonly class="form-control-plaintext" id="last_name" name="last_name" required>
                         </div>
                     </div>
                     <div class="form-row">
                         <label for="email" class="col-sm-4 col-form-label">Email</label>
                         <div class="col-sm-8">
-                            <input type="email" readonly class="form-control-plaintext" id="email" name="email" required>
+                            <input style="font-weight: bold;" type="email" readonly class="form-control-plaintext" id="email" name="email" required>
                         </div>
                     </div>
                     <div class="form-row">
                         <label for="phone" class="col-sm-4 col-form-label">Phone</label>
                         <div class="col-sm-8">
-                            <input type="text" readonly class="form-control-plaintext" id="phoneDetail" name="phone">
+                            <input style="font-weight: bold;" type="text" readonly class="form-control-plaintext" id="phoneDetail" name="phone">
                         </div>
                     </div>
                     <div class="form-row">
                         <label for="birthdate" class="col-sm-4 col-form-label">Birthdate</label>
                         <div class="col-sm-8">
-                            <input type="text" readonly class="form-control-plaintext" id="birthdateDetail" name="birthdate">
+                            <input style="font-weight: bold;" type="text" readonly class="form-control-plaintext" id="birthdateDetail" name="birthdate">
                         </div>
                     </div>
                     <div class="form-row">
                         <label for="address1" class="col-sm-4 col-form-label">Address 1</label>
                         <div class="col-sm-8">
-                            <input type="text" readonly class="form-control-plaintext" id="address1" name="address1">
+                            <input style="font-weight: bold;" type="text" readonly class="form-control-plaintext" id="address1" name="address1">
                         </div>
                     </div>
                     <div class="form-row">
                         <label for="address2" class="col-sm-4 col-form-label">Address 2</label>
                         <div class="col-sm-8">
-                            <input type="text" readonly class="form-control-plaintext" id="address2" name="address2">
+                            <input style="font-weight: bold;" type="text" readonly class="form-control-plaintext" id="address2" name="address2">
                         </div>
                     </div>
                     <div class="form-row">
                         <label for="city" class="col-sm-4 col-form-label">City</label>
                         <div class="col-sm-8">
-                            <input type="text" readonly class="form-control-plaintext" id="city" name="city">
+                            <input style="font-weight: bold;" type="text" readonly class="form-control-plaintext" id="city" name="city">
                         </div>
                     </div>
                     <div class="form-row">
                         <label for="state" class="col-sm-4 col-form-label">State</label>
                         <div class="col-sm-8">
-                            <input type="text" readonly  class="form-control-plaintext" id="state" name="state">
+                            <input style="font-weight: bold;" type="text" readonly  class="form-control-plaintext" id="state" name="state">
                         </div>
                     </div>
                     <div class="form-row">
                         <label for="zip" class="col-sm-4 col-form-label">Zip</label>
                         <div class="col-sm-8">
-                            <input type="text" readonly class="form-control-plaintext" id="zip" name="zip">
+                            <input style="font-weight: bold;" type="text" readonly class="form-control-plaintext" id="zip" name="zip">
                         </div>
                     </div>
                 </form>
